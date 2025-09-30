63164620a47d5ee2f3fcc6e5aa0daf37bec3d7cb
poornima825 <poornimaprasadini727@gmail.com>
2025-10-01 00:07:17 +0530

    comments delete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,8 @@
-/**
-* Template Name: Kelly
-* Template URL: https://bootstrapmade.com/kelly-free-bootstrap-cv-resume-html-template/
-* Updated: Aug 07 2024 with Bootstrap v5.3.3
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 
 (function() {
   "use strict";
 
-  /**
-   * Apply .scrolled class to the body as the page is scrolled down
-   */
   function toggleScrolled() {
     const selectBody = document.querySelector('body');
     const selectHeader = document.querySelector('#header');
